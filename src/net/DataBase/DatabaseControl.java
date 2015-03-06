cd550a07d276ce54db3fb5dfd50092ed68b0767e
lance1313 <jibijaco1421@gmail.com>
2015-03-06 13:34:26 -0700

    added doc

--- src/net/DataBase/DatabaseControl.java
+++ src/net/DataBase/DatabaseControl.java
@@ -176,6 +176,7 @@ public class DatabaseControl
 	public int describeTable()
 	{
 		int rowsAffected = 0;
+		//this is were the database.table rows and then the values
 		String insertQuery = "INSERT INTO ``.``() VALUES ();";
 		
 		try
